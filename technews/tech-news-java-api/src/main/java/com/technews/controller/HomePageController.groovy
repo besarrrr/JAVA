cffcbe4b2dca394ccccc7b73e31013df813e6ce8
Cesar Gonzalez <gcesar13@yahoo.com>
2022-11-17 20:14:42 -0700

    homepage controller

---
+++ technews/tech-news-java-api/src/main/java/com/technews/controller/HomePageController.groovy
@@ -0,0 +1,18 @@
+package com.technews.
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+}
